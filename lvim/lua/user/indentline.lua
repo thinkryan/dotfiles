0d85d2a469cd5ff576c6197f6fce5eebe82ed081
ryandennler <ryan.beatbox@gmail.com>
2022-08-04 14:17:01 -0600

    fix(indentline): mods for indentline
    This is WIP but adjusting and testing with some mods in IndentLine

--- lvim/lua/user/indentline.lua
+++ lvim/lua/user/indentline.lua
@@ -6,6 +6,20 @@ M.config = function()
     return
   end
 
+  vim.opt.termguicolors = true
+  -- Rainbow Indentation
+  -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
+  -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
+  -- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
+  -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
+  -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
+  -- vim.cmd vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+
+  -- Off pattern
+  vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
+  vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a gui=nocombine]]
+  vim.opt.list = true
+
   indent_blankline.setup {
     bufname_exclude = { "README.md" },
     buftype_exclude = { "terminal", "nofile" },
@@ -49,7 +63,7 @@ M.config = function()
     show_trailing_blanking_indent = false,
     show_first_indent_level = true,
     show_curent_context = true,
-    show_current_context_start = false,
+    show_current_context_start = true,
     use_treesitter = true,
     space_char_blankline = " ",
     context_patterns = {
@@ -59,7 +73,6 @@ M.config = function()
       "method",
       "^if",
       "^while",
-      "jsx_element",
       "^for",
       "^object",
       "^table",
@@ -68,26 +81,27 @@ M.config = function()
       "if_statement",
       "else_clause",
       "jsx_element",
+      "tsx_element",
+      "tsx_self_closing_element",
       "jsx_self_closing_element",
       "try_statement",
       "catch_clause",
       "import_statement",
       "operation_type",
     },
+    char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
+    space_char_highlight_list = {
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+    },
   }
 
-  -- vim.g.indent_blankline_char = "▎"
+  vim.g.indent_blankline_char = "▎"
   -- HACK: work-around for https://github.com/lukas-reineke/indent-blankline.nvim/issues/59
 
-  -- Rainbow Indentation Scaffold
-
-  -- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
-  -- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
-  -- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
-  -- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
-  -- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
-  -- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-  -- vim.opt.list = true
   -- vim.opt.listchars:append "space:⋅"
   -- vim.opt.listchars:append "space:"
   -- vim.opt.listchars:append "eol:↴"

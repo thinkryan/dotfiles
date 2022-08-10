45702587fcbfdbe35743cd2d1281916f1638ef2b
ryandennler <ryan.beatbox@gmail.com>
2022-08-09 22:53:05 -0600

    Kitty : Clean up Kitty configuration

--- kitty/functions/cht.fish
+++ kitty/functions/cht.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# TODO: Finish this and set it up with fzf to quickly lookup cht.sh results
+
 languages=$(echo "goalng c cpp typescript rust" | tr " " "\n")
 core_utils=$(echo "find xargs sed awk" | tr " " "\n")
 selected=$(echo -e "$languages\n$core_utils" | fzf)
